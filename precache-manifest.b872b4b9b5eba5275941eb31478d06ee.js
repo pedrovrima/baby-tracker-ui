708e51eaf30eef157276bdf690b4f63206883fc2
Pedro Martins <pedrovrima@gmail.com>
2021-02-22 20:17:36 -0300

    Updates

---
+++ precache-manifest.b872b4b9b5eba5275941eb31478d06ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38ac4a2f826b0aeca04b60e40459f30",
+    "url": "/baby-tracker-ui/index.html"
+  },
+  {
+    "revision": "ab188d19a0d600aa6343",
+    "url": "/baby-tracker-ui/static/css/main.ae0110a4.chunk.css"
+  },
+  {
+    "revision": "e982254eee598cdf4261",
+    "url": "/baby-tracker-ui/static/js/2.804dccc8.chunk.js"
+  },
+  {
+    "revision": "ce8bfac117b2c93690d0",
+    "url": "/baby-tracker-ui/static/js/3.5ca8d887.chunk.js"
+  },
+  {
+    "revision": "ab188d19a0d600aa6343",
+    "url": "/baby-tracker-ui/static/js/main.e3503bfa.chunk.js"
+  },
+  {
+    "revision": "4d42de86896c86aaca58",
+    "url": "/baby-tracker-ui/static/js/runtime~main.48490ad3.js"
+  },
+  {
+    "revision": "e36afe812b8c1c4458e7c542357384fa",
+    "url": "/baby-tracker-ui/static/media/loading.e36afe81.svg"
+  }
+]);
